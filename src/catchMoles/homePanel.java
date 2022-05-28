48b1801a20052f9e102c71f2fd7ec2d6dd7a36c3
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-05-28 16:18:45 +1200

    Main
    Main class created

---
+++ src/catchMoles/homePanel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catchMoles;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ssr7324
+ */
+public class homePanel extends JPanel{
+    public homePanel(){
+        setPreferredSize(new Dimension(600,600));
+        setBackground(Color.white);
+        setLayout(null);
+    }
+}
