47490cc6370dfab9437f2118559a64ce0615b286
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-05-28 19:43:22 +1200

    Squashed commit of the following:
    commit 43473840c7491810e42d562d2145e4b0e8739f77
    Author: 20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
    Date:   Sat May 28 16:42:39 2022 +1200
    
        Update Setting.java
    
    commit 5110d295fbf787f7f5381eb7b6a8edea072f102e
    Author: 20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
    Date:   Sat May 28 16:42:26 2022 +1200
    
        Create Setting
    
        Setting basic game.

---
+++ src/catchMoles/Setting.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catchMoles;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author ssr7324
+ */
+public class Setting {
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 600;
+    public static final int CHOSEN_MENU_NUMBER = 4;
+    public static final String[] MENU = {"Game Start", "Scoreboard", "Tutorial",  "Exit"};
+    
+    public static final int START = 0;
+    public static final int SCOREBOARD = 1;
+    public static final int TUTORIAL = 2;
+    public static final int EXIT = 3;
+    public static final int NOTHING = 4;
+    
+    public static final Color EXIT_HEAD = Color.cyan;
+    public static final Color EXIT_TAIL = Color.white;
+    public static final Color ENTER_HEAD = Color.BLUE;
+    public static final Color ENTER_TAIL = Color.BLACK;
+
+    public static final int NUMBER_MOLE = 9;
+    public static final int TIMER = 60;
+    public static final int ADD_SCORE = 10;
+    public static final int REMOVE_SCORE = -10;
+}
